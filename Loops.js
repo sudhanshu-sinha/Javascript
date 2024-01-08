511fe4309e390c53b07e79274c2751884d98db52
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2024-01-09 01:54:05 +0530

    no. of digits

--- Loops.js
+++ Loops.js
@@ -11,3 +11,15 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 console.log(arr)
+
+/* Write a JS program to find the no of digits in a number.
+Example : if number = 287152, count = 6 */
+
+let number = 287152
+let copy = number
+let count = 0
+while(copy>0){
+    count++
+    copy = Math.floor(copy/10)
+}
+console.log(count)
