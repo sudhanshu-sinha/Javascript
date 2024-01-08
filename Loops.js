150f6d0141cacf71e09bef5d4b60773f9c50ed8b
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2024-01-09 01:48:11 +0530

    delete all occurance

---
+++ Loops.js
@@ -0,0 +1,13 @@
+/* Write a JS program to delete all occurrences of element ‘num’ in a given array.
+Example : if arr = [1, 2, 3, 4, 5, 6, 2, 3] & num = 2
+Result should be arr = [1, 3, 4, 5, 6, 3] */
+
+const arr = [1, 2, 3, 4, 5, 6, 2, 3]
+const num = 2
+
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i]==num){
+        arr.splice(i,1)
+    }
+}
+console.log(arr)
