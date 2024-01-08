22cafba61f892ce2bd8f937b14e8eb33599dcd01
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2024-01-09 02:14:53 +0530

    Factorial

--- Loops.js
+++ Loops.js
@@ -29,3 +29,20 @@ while(copy>0){
 }
 console.log(count)
 console.log(sum)
+
+
+/* Print the factorial of a number n.
+[Factorial of a number n is the product of all positive integers less than or equal to a
+given positive integer and denoted by that integer. ]
+Example :
+7! (factorial of 7) = 1x2x3x4x5x6x7 = 5040
+5! (factorial of 5) = 1x2x3x4x5 = 120
+3! (factorial of 3) = 1x2x3 = 6
+0! Is always 1 */
+
+let x = 9
+let factorial = 1
+for (let i = 1; i <= x; i++) {
+    factorial *= i
+}
+console.log(factorial)
