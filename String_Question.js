3ff9719c5ea3a14a31b19de4318b05cd3b97c820
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2024-01-09 01:20:18 +0530

    String blank or not

---
+++ String_Question.js
@@ -0,0 +1,9 @@
+/* Write a JavaScript program to check whether a string is blank or not. */
+
+let str = "Sudhanshu"
+if (str.length==0) {
+    console.log("String is empty")
+}
+else{
+    console.log("String is not empty")
+}
