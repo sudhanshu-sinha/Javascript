9c35cca969acd3aa493aa605ca02bb7e3334ad3e
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2024-01-09 01:27:21 +0530

    lowercase or not

--- String_Question.js
+++ String_Question.js
@@ -1,9 +1,20 @@
 /* Write a JavaScript program to check whether a string is blank or not. */
 
-let str = "Sudhanshu"
+let str = "SudhAnshU"
 if (str.length==0) {
     console.log("String is empty")
 }
 else{
     console.log("String is not empty")
 }
+
+/*  Write a JavaScript program to test whether the character at the given (character)
+index is lower case. */
+const index = 4
+if(str[index]==str[index].toLocaleLowerCase()){
+    console.log("Character is in lower case")
+}
+else{
+    console.log("Chater is not in lower case")
+}
+
