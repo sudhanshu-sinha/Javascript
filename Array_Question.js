af5790e00f176920a494bb56f4d1688ce8a3c5f5
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2024-01-09 01:11:32 +0530

    slice last n number

--- Array_Question.js
+++ Array_Question.js
@@ -7,5 +7,10 @@ const array = [7, 9, 0, -2]
 const n = 3
 console.log(array.slice(0,n))
 
+/*  Write a JavaScript program to get the last n elements of an array. [n can be any
+positive number].
+For example: for array [7, 9, 0, -2] and n=3
+Print, [9, 0, -2] */
 
+console.log(array.slice(array.length-n))
 
