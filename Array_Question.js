e9777a843622774aa18f92946c718cfc3145c2b3
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2024-01-09 01:10:58 +0530

    slice first n number

---
+++ Array_Question.js
@@ -0,0 +1,11 @@
+/*Write a JavaScript program to get the first n elements of an array. [n can be any
+positive number].
+For example: for array [7, 9, 0, -2] and n=3
+Print, [7, 9, 0]*/ 
+
+const array = [7, 9, 0, -2]
+const n = 3
+console.log(array.slice(0,n))
+
+
+
