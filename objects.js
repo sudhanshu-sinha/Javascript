e30c6f6453914bca6bc85ce7605bc12b68ad1c99
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2024-01-13 23:16:40 +0530

    function inside obj

--- objects.js
+++ objects.js
@@ -1,11 +1,24 @@
 // objects
-let students = {
+let student = {
     name: "Sudhanshu Sinha",
     uid: "21BCS9735",
     age: 19
 }
 
-students.name = "Sunny Sinha"
+student.name = "Sunny Sinha"
 
-console.log(students.name)
-console.log(students)
+console.log(student.name)
+console.log(student)
+
+let employee = {
+    firstName: "Sudhanshu",
+    lastName: "Sinha",
+    uid: "21BCS9735",
+    age: 19,
+    fullName: function() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+console.log(employee.fullName())
+
