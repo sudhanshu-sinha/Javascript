ec49d30410275d60e61340309c06525147047e9a
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2024-01-13 23:05:20 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,11 @@
+// objects
+let students = {
+    name: "Sudhanshu Sinha",
+    uid: "21BCS9735",
+    age: 19
+}
+
+students.name = "Sunny Sinha"
+
+console.log(students.name)
+console.log(students)
